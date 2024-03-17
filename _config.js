6664c4b50f1d3360c204967d83a5c82ba7280700
egcharchi <eric.charchi@student.moringaschool.com>
2024-03-17 18:53:57 +0300

    added demo files

---
+++ _config.js
@@ -0,0 +1,9 @@
+var config = {}
+
+// Update to have your correct username and password
+config.mongoURI = {
+    production: 'mongodb+srv://morip01:QwOi07IsVywGlv0B@moringaip1.mqq3hpt.mongodb.net/?retryWrites=true&w=majority',
+    development: 'mongodb+srv://morip01:QwOi07IsVywGlv0B@moringaip1.mqq3hpt.mongodb.net/?retryWrites=true&w=majority',
+    test: 'mongodb+srv://morip01:QwOi07IsVywGlv0B@moringaip1.mqq3hpt.mongodb.net/?retryWrites=true&w=majority',
+}
+module.exports = config;
---
+++ routes/image.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const router = express.Router();
+let Image = require('../models/images');
+
+router.get('/:id', (req,res)=>{
+    // console.log(req);
+    Image.findById(req.params.id,function(err, image){
+        if (err) console.log(err)
+        // console.log(image);
+        res.render('singleImage', {title: 'Single Image', image:image})
+    } )
+})
+
+router.put('/:id', (req,res) =>{
+    console.log(req.params.id)
+    console.log(req.body);
+    Image.updateOne({_id:req.params.id},{
+        $set:{
+            name:req.body.name
+        }
+    },{upsert: true}, function(err,image ){
+        if (err) console.log(err)
+        res.redirect('/')
+    })
+})
+
+router.delete('/:id', (req,res) =>{
+    console.log(req.params.id)
+
+    Image.deleteOne({_id: req.params.id}, function(err){
+        if (err) console.log(err)
+        res.redirect('/index')
+    })
+})
+
+module.exports = router
